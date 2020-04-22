ca158c5d8eded59a7ca9b629a264223f8b3814ec
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-04-21 22:21:00 -0300

    add config style main  page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositorio" />
+        <SubmitButton disable>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
